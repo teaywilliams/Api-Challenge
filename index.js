a0dcb9fa7e350953e8feb6ab428555d5786a0248
teaywilliams <73564768+teaywilliams@users.noreply.github.com>
2020-12-15 19:42:58 -0500

    intital push

---
+++ index.js
@@ -0,0 +1,36 @@
+const URL= "https://picsum.photos/v2/list?page=2&limit=40";
+const lorem = document.querySelector(".lorem");
+
+fetch(URL)
+.then ((result) => {
+  console.log(result);
+  return result.json();
+})
+.then((data)=> {
+  displayImages(data);
+  console.log(data);
+})
+
+function displayImages(imagesPassingThrough) {
+  console.log(imagesPassingThrough);
+  imagesPassingThrough.forEach((element) => {
+    
+    let imageDiv = document.createElement("div");
+    let imageTag = document.createElement("img");
+    let imageAuthor = document.createElement("h6");
+
+    imageDiv.setAttribute("class", "img-div");
+    imageTag.setAttribute("class","imageClass");
+    imageAuthor.setAttribute("class", "author");
+
+    imageTag.src = element.download_url;
+    imageAuthor.textContent = element.author;
+    
+    // console.log(element.download_url);
+
+    lorem.appendChild(imageDiv);
+    imageDiv.appendChild(imageTag);
+    imageDiv.appendChild(imageAuthor);
+  });
+}
+
